ad13980aa605be4daf5ce99b9913ca4662b6101c
diaojieyuan <diaojieyuan@gotion.com.cn>
2022-05-01 17:00:06 +0800

    过滤两个项目电压与温度数组元素长度，与项目保持一致

--- src/main/scala/com/neuexample/vehicle/Chery.scala
+++ src/main/scala/com/neuexample/vehicle/Chery.scala
@@ -9,28 +9,31 @@ object Chery {
    *
    * @param json
    */
-  def processCheryDataDJ1902(json: JSONObject): Unit ={
+  def processCheryDataDJ1902(json: JSONObject): Unit = {
     //总电流数据处理totalCurrent->current
     var probeTeptureArray: Array[Int] = stringToIntArray(json.getString("probeTemperatures"))
     var cellVoltageArray: Array[Int] = stringToIntArray(json.getString("cellVoltages"))
-    if(probeTeptureArray !=null && probeTeptureArray.length >28){
-      probeTeptureArray = probeTeptureArray.slice(0,28)
-      json.put("probeTemperatures",JSON.toJSON(probeTeptureArray))
+    if (probeTeptureArray != null && probeTeptureArray.length > 28) {
+      probeTeptureArray = probeTeptureArray.slice(0 , 28)
+      json.put("probeTemperatures", JSON.toJSON(probeTeptureArray))
     }
-    if(cellVoltageArray !=null && cellVoltageArray.length>106){
-      cellVoltageArray = cellVoltageArray.slice(0,106)
-      json.put("cellVoltageArray",JSON.toJSON(cellVoltageArray))
+    if (cellVoltageArray != null && cellVoltageArray.length > 106) {
+      cellVoltageArray = cellVoltageArray.slice(0 , 106)
+      json.put("cellVoltageArray", JSON.toJSON(cellVoltageArray))
     }
-
   }
 
-  def processCheryDataDJ2015(json: JSONObject): Unit ={
+  def processCheryDataDJ2015(json: JSONObject): Unit = {
     //总电流数据处理totalCurrent->current
     var probeTeptureArray: Array[Int] = stringToIntArray(json.getString("probeTemperatures"))
-    if(probeTeptureArray !=null && probeTeptureArray.length >21){
-      probeTeptureArray = probeTeptureArray.slice(0,21)
-      json.put("probeTemperatures",JSON.toJSON(probeTeptureArray))
+    var cellVoltageArray: Array[Int] = stringToIntArray(json.getString("cellVoltages"))
+    if (probeTeptureArray != null && probeTeptureArray.length > 21) {
+      probeTeptureArray = probeTeptureArray.slice(0 , 21)
+      json.put("probeTemperatures", JSON.toJSON(probeTeptureArray))
     }
-
+    if (cellVoltageArray != null && cellVoltageArray.length > 108) {
+      cellVoltageArray = cellVoltageArray.slice(0 , 108)
+      json.put("cellVoltageArray", JSON.toJSON(cellVoltageArray))
+    }
   }
 }
