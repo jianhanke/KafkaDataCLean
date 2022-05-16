2c9b62624c65ad099e5148290549a9d6ae7a238b
zhengzhonghua <zhengzhonghua@gotion.com.cn>
2022-05-16 15:37:36 +0800

    修改吉利和五菱绝缘阻值从欧姆到千欧

--- src/main/scala/com/neuexample/streaming/CleanStreaming.scala
+++ src/main/scala/com/neuexample/streaming/CleanStreaming.scala
@@ -59,14 +59,17 @@ object CleanStreaming extends Serializable {
         }else if(vehicleFactory == 2){
           isDelete = isDeleteBasedOnCommon(old_obj, new_obj);
         }
-        println(isDelete)
+//        println(isDelete)
     }
 
     if(isDelete){
       null    //删除当前数据
     }else{   // 保留下来的
       val json: JSONObject = JSON.parseObject(new_obj.toString)    // 必须转化成新的
-
+      //处理吉利soc跳变问题
+//      if(vehicleFactory ==5){
+//        json = GeelySocJump(old_obj, new_obj)
+//      }
       if(vehicleFactory == 2){
         processJhData(json)
       }
@@ -94,7 +97,7 @@ object CleanStreaming extends Serializable {
 
       cleanArrayValue(json)
 
-      state.update(json);
+      state.update(json)
 
       json.toString
     }
